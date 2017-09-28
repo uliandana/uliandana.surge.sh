dbd12f97d7c5244872f0af0ca80c8738a7229c27
Oka Uliandana <oka.uliandana@gmail.com>
2017-09-28 17:33:56 +0700

    Theme color meta

--- reading-list/src/script/util/Util.ts
+++ reading-list/src/script/util/Util.ts
@@ -1,5 +1,5 @@
 export default class Util {
-    static assetsUrl: string = "http://127.0.0.1:8090";
+    static assetsUrl: string = "http://okulassets.surge.sh";
 
     static textEncode(text: string) {
         return text.toLowerCase().replace(/[^0-9A-Za-z\-\s]/g, '').replace(/\-/g, ' ').replace(/\s+/g, '-').replace(/\-$/g, '');
